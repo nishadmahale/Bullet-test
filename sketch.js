3a9d6e7c1d17d005f1aac3439bf604db3cd46f2c
nishadmahale <70054272+nishadmahale@users.noreply.github.com>
2020-09-08 17:20:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,12 +8,13 @@ var bullet,speed,weight;
 
 function setup () {
     createCanvas(1600,800);
+    
     speed=random(223,321);
     weight=random(30,52);
     thickness=random(22,83);
 
-    bullet=createSprite(50,200,50,50);
-    wall=createSprite(1200,200,60,thickness,height/2);
+    bullet=createSprite(50,200,50,50,thickness.height/2);
+    wall=createSprite(1200,200,60);
     
 
 
