3fb7af737cfde3dba7ccba7223aa752bfb3f00f1
nishadmahale <70054272+nishadmahale@users.noreply.github.com>
2020-09-08 17:19:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,17 +7,14 @@ var bullet,speed,weight;
 
 
 function setup () {
-    createCanvas(1600,800)
-    
+    createCanvas(1600,800);
     speed=random(223,321);
     weight=random(30,52);
     thickness=random(22,83);
-    bullet.velocityX=speed;
-    
+
     bullet=createSprite(50,200,50,50);
     wall=createSprite(1200,200,60,thickness,height/2);
     
-   
 
 
 
@@ -26,7 +23,7 @@ function setup () {
 
 
 function draw(){
-    background("yellow");
+    background("green");
     
     if(hasColided (bullet,wall)) {
         
@@ -35,10 +32,10 @@ function draw(){
        var  damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
         
     if(damage>10){
-            bullet.shapeColor= colour(255,0,0);
+            wall.shapeColor= colour(255,0,0);
        }
     if (damage<10){
-       bullet.shapeColor=colour(0,255,0);
+       wall.shapeColor=colour(0,255,0);
       }
     
     }
