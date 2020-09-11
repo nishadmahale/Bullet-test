7e4c7d2e4099018bfbc684013e4c5776bb57db95
nishadmahale <70054272+nishadmahale@users.noreply.github.com>
2020-09-11 18:53:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,6 @@ function setup () {
 
      createCanvas(1600,800);
     
-    bullet=createSprite(50,200,50,50);
-    wall=createSprite(1500,200,60,height/2);
     
     
     speed=random(223,321);
